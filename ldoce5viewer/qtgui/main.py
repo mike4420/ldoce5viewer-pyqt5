02bd2f443bd0befcc4324a20cab48dd2a7a87451
JayT <johannes.titz@gmail.com>
2017-07-12 17:24:54 +0200

    fix audio file saving
    File saving gives an error, because the filename consists of two
    strings. To fix it just use the first element filename[0] and also
    change type from "w" to "wb" (write binary)

--- ldoce5viewer/qtgui/main.py
+++ ldoce5viewer/qtgui/main.py
@@ -558,7 +558,7 @@ class MainWindow(QMainWindow):
         def showSaveDialog(data):
             filename = QFileDialog.getSaveFileName(self,  u'Save mp3',  '',  u'MP3 Files (*.mp3)')
             if filename != '':
-                file = open(filename, "w")
+                file = open(filename[0], "wb")
                 file.write(data)
                 file.close()
         self._getAudioData(path, showSaveDialog)
